697b235b3733be0c1a02772103d58324f1214ebf
Eric Chiu <c3366301@gmail.com>
2018-05-08 17:17:12 +0800

    add loca directory and Todo's Helper Controller config

---
+++ app/code/local/Todo/Example/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Todo_Example_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Todo/Example/controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+class Todo_Example_HelloController extends Mage_core_controller_Front_Action
+{
+  public function indexAction()
+ {
+  echo "sss";
+}
+  public function worldAction()
+  {
+    echo 'Hello, World!';
+  }
+}
